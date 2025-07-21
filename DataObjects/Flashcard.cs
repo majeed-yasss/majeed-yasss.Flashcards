fcd49047610a5cf9284e9369704c1e54d5bf84d1
majeed-yasss <majeed-yass@SomePlace>
2025-07-21 22:10:36 +0300

    DataObjects class library

---
+++ DataObjects/Flashcard.cs
@@ -0,0 +1,9 @@
+﻿namespace DataObjects;
+
+public class Flashcard
+{
+    public int Id { get; set; }
+    public int StackID { get; set; }
+    public string Front { get; set; }
+    public string Back { get; set; }
+}
---
+++ DataObjects/StudySession.cs
@@ -0,0 +1,10 @@
+﻿namespace DataObjects;
+public class StudySession
+{
+    public int Id { get; set; }
+    public int StackID { get; set; }
+    public int Answerd { get; set; }
+    public int Total { get; set; }
+    public DateTime Started { get; set; }
+    public DateTime Ended { get; set; }
+}
