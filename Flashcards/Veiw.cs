2c05f7aebaa14404e119f9688d0da3f240ab5696
majeed-yasss <majeed-yass@SomePlace>
2025-07-22 15:09:30 +0300

    introducing View

---
+++ Flashcards/Veiw.cs
@@ -0,0 +1,29 @@
+﻿using DataObjects;
+using Spectre.Console;
+namespace Flashcards;
+internal class Veiw
+{
+    public static Options.WelcomeMenu WelcomeMenu()
+    {
+        Rule rule = new("Welcome!");
+        rule.LeftJustified();
+
+        string option = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .AddChoices(Options.WelcomeMenuLabels.Keys));
+
+        return Options.WelcomeMenuLabels[option];
+    }
+
+    public static Options.MainMenu MainMenu()
+    {
+        Rule rule = new("Main Menu");
+        rule.LeftJustified();
+
+        string option = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .AddChoices(Enum.GetNames<Options.MainMenu>()));
+
+        return Enum.Parse<Options.MainMenu>(option);
+    }
+}
